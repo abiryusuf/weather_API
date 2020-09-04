c77b72df32e11f6b456de8df9db01e37037f7d1f
abiryusuf <yusuf_ctg@yahoo.com>
2020-09-04 18:46:34 -0500

    get api key

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
-function doSometing(){
-    fetch("http://www.boredapi.com/apiactivity/")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
-}
+   const apiKey = '64c4c4cc93f3761cfa9d80b9637e1c06';
+   const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
 
-doSometing();
+function getWetherInfo(currentLoc){
+   const url = `${apiBase}?q=${currentLoc}&appid=${apiKey}`;
+       fetch(url)
+           .then(response => response.json())
+           .then(data =>
+               console.log(data));
+           
+}
+const serachBtn = document.getElementById("search_button");
+serachBtn.addEventListener("click", function(){
+    console.log("click")
+})
+
+getWetherInfo("New York");
