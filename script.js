9c82442d0856dc58d039f85ee53262e76e4e927f
abiryusuf <yusuf_ctg@yahoo.com>
2020-09-04 19:37:18 -0500

    get weather info

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+    const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
+    const apiKey = '64c4c4cc93f3761cfa9d80b9637e1c06';
+    
 
-   const apiKey = '64c4c4cc93f3761cfa9d80b9637e1c06';
-   const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
+    function getWetherInfo(currentLoc){
+        // api.openweathermap.org/data/2.5/weather?q={current location}&appid={your api key}
+    const url = `${apiBase}?q=${currentLoc}&appid=${apiKey}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => 
+              //console.log(data))
+             getData(data))
+            
+            
+    }
+    const serachBtn = document.getElementById("search_button");
+    serachBtn.addEventListener("click", function(){
+        //console.log("click")
+        const city = document.getElementById("city").value;
+        getWetherInfo(city);
+    })
 
-function getWetherInfo(currentLoc){
-   const url = `${apiBase}?q=${currentLoc}&appid=${apiKey}`;
-       fetch(url)
-           .then(response => response.json())
-           .then(data =>
-               console.log(data));
-           
-}
-const serachBtn = document.getElementById("search_button");
-serachBtn.addEventListener("click", function(){
-    console.log("click")
-})
+    function getData(data){
+        document.getElementById("show_city").innerText = data.name;
+        document.getElementById("show_temperature").innerText = data.main.temp;
+        document.getElementById("weather_status").innerText = data.weather[0].description;
+        document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+        document.getElementById("city").value = "";
+    }
 
 getWetherInfo("New York");
